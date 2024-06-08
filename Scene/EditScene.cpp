b8a6f6e2a98aecb848a8a8588060f11bda8f9b99
thanksone <hi.thanksone@gmail.com>
2024-06-09 01:35:12 +0800

    douo

--- Scene/EditScene.cpp
+++ Scene/EditScene.cpp
@@ -1,3 +1,58 @@
-//
-// Created by thanksone on 6/8/24.
-//
+#include <allegro5/allegro.h>
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <functional>
+#include <vector>
+#include <queue>
+#include <string>
+#include <memory>
+#include<iostream>
+
+#include "Engine/AudioHelper.hpp"
+#include "UI/Animation/DirtyEffect.hpp"
+#include "Engine/GameEngine.hpp"
+#include "Engine/Group.hpp"
+#include "UI/Component/Label.hpp"
+#include "UI/Animation/Plane.hpp"
+#include "EditScene.hpp"
+
+void EditScene::Initialize(){
+    doko = 0;
+}
+void EditScene::Terminate(){
+
+}
+void EditScene::Update(float deltatime){
+
+}
+void EditScene::Draw() const{
+
+}
+void EditScene::OnMouseDown(int button, int mx, int my) {
+
+}
+void EditScene::OnMouseMove(int mx, int my){
+
+}
+void EditScene::OnMouseUp(int button, int mx, int my){
+
+}
+void EditScene::OnKeyDown(int keyCode){
+    if(!doko) return;
+    IScene::OnKeyDown(keyCode);
+    if (keyCode >= ALLEGRO_KEY_0 && keyCode <= ALLEGRO_KEY_9) {
+        inf[gon][doko] *= 10;
+        inf[gon][doko] +=('0'+keyCode-ALLEGRO_KEY_0);
+    }
+    else if(keyCode >= ALLEGRO_KEY_PAD_0 && keyCode <= ALLEGRO_KEY_PAD_9) {
+        inf[gon][doko] *= 10;
+        inf[gon][doko] +=('A'+keyCode-ALLEGRO_KEY_PAD_0);
+    }
+    else if(keyCode == ALLEGRO_KEY_BACKSPACE) {
+        inf[gon][doko] /= 10;
+    }
+    RemoveObject(namae->GetObjectIterator());
+    namae=new Engine::Label(name,"pirulen.ttf", 48, Engine::GameEngine::GetInstance().GetScreenSize().x/2-200, Engine::GameEngine::GetInstance().GetScreenSize().y/8+50, 255, 255, 255, 255, 0, 0.5);
+    addObject(1,namae);
+}
--- Scene/EditScene.hpp
+++ Scene/EditScene.hpp
@@ -1,4 +1,32 @@
 #ifndef EDITSCENE_HPP
 #define EDITSCENE_HPP
-
+#include <allegro5/allegro_audio.h>
+#include <list>
+#include <memory>
+#include <utility>
+#include <vector>
+#include <string>
+#include "Engine/IScene.hpp"
+#include "Engine/Point.hpp"
+class EditScene final : public Engine::IScene {
+private:
+    ALLEGRO_SAMPLE_ID bgmId;
+    std::shared_ptr<ALLEGRO_SAMPLE_INSTANCE> deathBGMInstance;
+    int gon, doko, pi;
+    std::vector<std::vector<int>> inf;
+protected:
+    int SpeedMult;
+public:
+    static const int MapWidth, MapHeight;
+    int MapId;
+    explicit EditScene() = default;
+    void Initialize() override;
+    void Terminate() override;
+    void Update(float deltaTime) override;
+    void Draw() const override;
+    void OnMouseDown(int button, int mx, int my) override;
+    void OnMouseMove(int mx, int my) override;
+    void OnMouseUp(int button, int mx, int my) override;
+    void OnKeyDown(int keyCode) override;
+};
 #endif //EDITSCENE_HPP
