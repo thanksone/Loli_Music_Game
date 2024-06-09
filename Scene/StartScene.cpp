2b36849a565ee0d54d742c8e698a14d0ddfe9975
gugugugugugugu <u.6ru8fu8fu6@gmail.com>
2024-06-09 23:13:26 +0800

    ouob

--- Scene/StartScene.cpp
+++ Scene/StartScene.cpp
@@ -31,7 +31,7 @@ void StartScene::Initialize() {
     std::string account,sanity,maxsan;
     std::string filename="Resource/account-status/guest.txt";
     std::ifstream fin(filename);
-    sancheck="good";
+
     fin>>account>>sanity>>maxsan;
     if(std::stoi(sanity)<std::stoi(maxsan)/5) {
         sancheck="bad";
@@ -43,27 +43,31 @@ void StartScene::Initialize() {
     fin.close();
     std::string dirtimg="stage-select/san" +sancheck+"dirt.png";
     std::string floorimg="stage-select/san" +sancheck+"floor.png";
-    if(sancheck=="bad")
-        AddNewObject(new Engine::Label("Abyssal Tunes", "WOODCUTTER-BCN-Style-1.ttf", 120, halfW, halfH / 3 + 50, 125,30,32, 255, 0.5, 0.5));
-    else
-        AddNewObject(new Engine::Label("Abyssal Tunes", "WOODCUTTER-BCN-Style-1.ttf", 120, halfW, halfH / 3 + 50, 125,30,32, 255, 0.5, 0.5));
+    std::string fontname;
+    if(sancheck=="bad"){
+        fontname="Raslani-Kavaliar-Kaiser-1.ttf";
+        AddNewObject(new Engine::Label("Abyssal Tunes", fontname, 120, halfW, halfH / 3 + 50, 125,30,32, 255, 0.5, 0.5));
+    }
+    else{
+        fontname="Raslani-Kavaliar-Kaiser-1.ttf";
+        AddNewObject(new Engine::Label("Abyssal Tunes", fontname, 120, halfW, halfH / 3 + 50, 125,30,32, 255, 0.5, 0.5));
+    }
 
 
-
-    btn = new Engine::ImageButton(dirtimg, floorimg, halfW - 200, halfH / 2 + 200 , 400, 150);
+    btn = new Engine::ImageButton(dirtimg, floorimg, halfW -150, halfH / 2 + 200 , 300, 150);
     btn->SetOnClickCallback(std::bind(&StartScene::PlayOnClick, this, 1));
     AddNewControlObject(btn);
-    AddNewObject(new Engine::Label("Play", "WOODCUTTER-BCN-Style-1.ttf", 48, halfW, halfH / 2 + 275, 125, 30, 32, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("Play", fontname, 48, halfW, halfH / 2 + 275, 125, 30, 32, 255, 0.5, 0.5));
 
-    btn = new Engine::ImageButton(dirtimg, floorimg, halfW - 200, halfH * 3 / 2 - 50, 400, 150);
+    btn = new Engine::ImageButton(dirtimg, floorimg, halfW - 150, halfH * 3 / 2 - 50, 300, 150);
     btn->SetOnClickCallback(std::bind(&StartScene::SettingsOnClick, this, 2));
     AddNewControlObject(btn);
-    AddNewObject(new Engine::Label("Settings", "WOODCUTTER-BCN-Style-1.ttf", 48, halfW, halfH * 3 / 2+25, 125, 30, 32, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("Settings", fontname, 48, halfW, halfH * 3 / 2+25, 125, 30, 32, 255, 0.5, 0.5));
 
-    btn = new Engine::ImageButton(dirtimg, floorimg,  w- 450, halfH * 3 / 2 +50, 400, 150);
+    btn = new Engine::ImageButton(dirtimg, floorimg,  w- 400, halfH * 3 / 2 +50, 300, 150);
     btn->SetOnClickCallback(std::bind(&StartScene::EditOnClick, this));
     AddNewControlObject(btn);
-    AddNewObject(new Engine::Label("Edit mode", "WOODCUTTER-BCN-Style-1.ttf", 48, w-250, halfH * 3 / 2+100+25, 125, 30, 32, 255, 0.5, 0.5));
+    AddNewObject(new Engine::Label("Edit mode", fontname, 48, w-250, halfH * 3 / 2+100+25, 125, 30, 32, 255, 0.5, 0.5));
 
 }
 void StartScene::Terminate() {
