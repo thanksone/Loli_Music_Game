f2c7f6c6c6c82e99462f7e6ed5c9990eb59856e9
thanksone <hi.thanksone@gmail.com>
2024-06-22 20:38:56 +0800

    douo

--- Character/Loli.cpp
+++ Character/Loli.cpp
@@ -1,6 +1,6 @@
 #include "Loli.hpp"
 Loli::Loli(std::string namae, int san, int fullsan, bool wind): name(namae), san(san), fullsan(fullsan), wind(wind){}
 void Loli::draw(Engine::IScene *scene, float x, float y, float w, float h, float anchorX, float anchorY){
-    std::string tachie = (wind? "sanbad" : "sangood") + name;
+    std::string tachie = (wind? "characters/sanbad" : "characters/sangood") + name + ".png";
     scene->AddNewObject(new Engine::Image(tachie, x, y, w, h, anchorX, anchorY));
 }
